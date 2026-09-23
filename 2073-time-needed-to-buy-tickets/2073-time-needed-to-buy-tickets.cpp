d493c1a58c9468a5d9a83221f0fe65c8e0a14cef
Rudranshtripathigit <tripathirudransh15@gmail.com>
2026-09-24 01:01:30 +0530

    Time: 0 ms (100%), Space: 11.3 MB (24.27%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -5,15 +5,15 @@ public:
         for(int i=0;i<tickets.size();i++){
             q.push(i);
         }
-        int time =0;
+        int time=0;
         while(tickets[k]!=0){
-            int i = q.front();
-           q.pop();  // front wale ko hatao
-            tickets[i]--; // 1 person = 1ticket at a time
-            time++; // 1 tick = 1sec
-
-          if (tickets[i]!=0)
-                q.push(i);
+            tickets[q.front()]--;
+            time++;
+            
+            if(tickets[q.front()]){
+                q.push(q.front());
+            }
+            q.pop();
         }
         return time;
     }
